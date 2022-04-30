d5b43291c0785c7b3aad1b60af9d70aae2a31b60
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-30 19:51:16 +0300

    nodemailer configuration initialized

--- server/src/utils/nodemailer/nodemailer.utils.js
+++ server/src/utils/nodemailer/nodemailer.utils.js
@@ -1,7 +1,5 @@
 const nodemailer = require("nodemailer");
-const CustomError = require("../../errors");
-const crypto = require("crypto");
-const sendEmail = async (email) => {
+const sendEmail = async (authKey, email) => {
 
     const transporter = nodemailer.createTransport({
         host: process.env.ETHEREAL_HOST,
@@ -12,25 +10,18 @@ const sendEmail = async (email) => {
         }
     });
 
-    try {
-        const verificationToken = crypto.randomBytes(40).toString("hex");
-        // Change the URL to FE domain.
-        const webURL = "http://localhost:3000";
-        // Change the url to web url.
-        await transporter.sendMail({
-            from: "\"Fred Foo 👻\" <foo@example.com>", // sender address
-            to: email,
-            subject: "Password reset", // Subject line
-            html: "<h4>Hello,</h4>" +
-                "<p>We have received your request to reset password. Please follow the provided link. " +
-                `<strong><a href='${webURL}/password-reset?key=${verificationToken}?email=${email}'>Password reset</a></strong></p>`, // html body
-        });
-    } catch (e) {
-        console.error("Error occurred on sending email. Handle incoming error request by e.code");
-        if (e) throw new CustomError.InternalServer("Something went wrong... Please try again later");
-    }
 
+    const webURL = "http://localhost:8080";
 
+    // send the email
+    await transporter.sendMail({
+        from: "\"Fred Foo 👻\" <foo@example.com>",
+        to: email,
+        subject: "Password reset",
+        html: "<h4>Hello,</h4>" +
+            "<p>We have received your request to reset password. Please follow the provided link. " +
+            `<strong><a href='${webURL}/forgot-password?authKey=${authKey}?email=${email}'>Password reset</a></strong></p>`, // html body
+    });
 };
 
 module.exports = {sendEmail};
