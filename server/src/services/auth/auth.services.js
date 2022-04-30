3916c755cbc0c48f9c1e08e938d82477267d840e
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-30 19:51:54 +0300

    forget password service completed

--- server/src/services/auth/auth.services.js
+++ server/src/services/auth/auth.services.js
@@ -4,6 +4,7 @@ const CustomError = require("../../errors");
 const {hashPassword, validatePassword} = require("../../utils/bcrypt/bcrypt.utils");
 const {assignCookiesToResponse, deleteCookiesFromResponse} = require("../../utils/jwt/jwt.utils");
 const {sendEmail} = require("../../utils/nodemailer/nodemailer.utils");
+const crypto = require("crypto");
 const registerService = async (requestBody) => {
     const {email, username, password, firstName, lastName} = requestBody;
 
@@ -75,12 +76,122 @@ const logoutService = async (Response) => {
     return {message: "User successfully logged out"};
 };
 
-const forgotPasswordService = async (requestBody) => {
-    const {email} = requestBody;
+const forgotPasswordService = async (Request) => {
+    const {password, email: bodyEmail} = Request.body;
+    const {authKey, email: queryEmail} = Request.query;
 
-    await sendEmail(email);
+    // check for existing user
+    const userExists = await prisma.user.findFirst({
+        where: {
+            email: bodyEmail,
+        }
+    });
 
-    return {message: "Instructions how to reset password are sent"};
+    // email exists
+    if (userExists) {
+
+        // user opened the url from email, but didn't typed the password value
+        if (authKey && queryEmail && !password || authKey && queryEmail && password.trim() === "") {
+            throw new CustomError.BadRequest("Password is required");
+        }
+
+        // user opened the url from email, passed all values
+        if (authKey && queryEmail && password) {
+            // find the request author by authKey and email
+            const existingPasswordRequest = await prisma.Reset_Password.findFirst({
+                where: {
+                    authKey,
+                    userEmail: queryEmail.toLowerCase()
+                }
+            });
+
+            if (existingPasswordRequest) {
+                return await resetPasswordHelper(existingPasswordRequest, password);
+            } else {
+                return {message: "Password reset has expired, please request one more time"};
+            }
+
+        }
+
+        // user only passed his email
+        // generate authKey
+        const authenticationKey = crypto.randomBytes(10).toString("hex");
+
+        // save request to DB
+        const resetPasswordUser = await prisma.Reset_Password.create({
+            data: {
+                userEmail: bodyEmail.toLowerCase(),
+                authKey: authenticationKey,
+                expiresAt: new Date(+new Date() + 60000 * 10)
+            },
+        });
+
+        if (resetPasswordUser) {
+            // call email services
+            await sendEmail(authenticationKey, bodyEmail.toLowerCase());
+            return {message: "If email exists instructions how to reset password will be sent to the email and will be valid for 10 minutes"};
+        } else {
+            throw new CustomError.BadRequest("Something went wrong... Please try again later");
+        }
+
+    } else {
+        return {message: "If email exists instructions how to reset password will be sent to the email and will be valid for 10 minutes"};
+    }
+};
+
+const resetPasswordHelper = async (existingPasswordRequest, password) => {
+        // is the request still valid? check expiry date
+        const isResetValid = await compareExpirationTime(existingPasswordRequest);
+
+        // request is valid - reset password
+        if (isResetValid) {
+            const hashedPassword = await hashPassword(password);
+
+            // update users password
+            const updatedUser = await prisma.user.update({
+                where: {
+                    email: existingPasswordRequest.userEmail
+                },
+                data: {
+                    password: hashedPassword,
+                }
+            });
+
+            // if successfully updated - remove the records related to this user
+            if (updatedUser) {
+                const deletedUserRecords = await prisma.Reset_Password.deleteMany({
+                    where: {
+                        userEmail: existingPasswordRequest.userEmail
+                    }
+                });
+
+                if (deletedUserRecords.count > 0) {
+                    return {message: "Password successfully updated"};
+                }
+            } else throw new CustomError.BadRequest("Something went wrong... Please try again later");
+        }
+    }
+;
+
+const compareExpirationTime = async (expirationTime) => {
+    // get expiry date
+    const expiresAt = expirationTime.expiresAt.toLocaleTimeString("it-IT", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+
+    // current time
+    const currentTime = new Date().toLocaleTimeString("it-It", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+
+    // is the request still valid?
+    if (currentTime > expiresAt) throw new CustomError.BadRequest("Password reset expired. Please request one more time");
+
+    return true;
 };
 
 module.exports = {registerService, loginService, logoutService, forgotPasswordService};
